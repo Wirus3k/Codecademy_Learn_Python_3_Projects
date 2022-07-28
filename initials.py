4704032714ec4e5272d6864f22f776d4652a541b
Pawel Wira <108659378+Wirus3k@users.noreply.github.com>
2022-07-28 13:43:47 +0200

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,20 @@
+# Pawel Wira
+# I am big fan of FC Barcelona
+print( """ SSS   L
+S   S  L
+S      L
+ SSS   L
+    S  L
+S   S  L
+ SSS   LLLLL""")
+
+name = """ 
+  SSS   L
+ S   S  L
+ S      L   
+  SSS   L
+     S  L
+ S   S  L
+  SSS   LLLLL
+"""
+print (name)
